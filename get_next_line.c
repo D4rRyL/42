9b30f2c89caf532a00cdc329c84dcc2b969ed67e
chbechet <chbechet@student.42.fr>
2017-02-26 19:24:02 +0100

    test home

--- get_next_line.c
+++ get_next_line.c
@@ -6,56 +6,52 @@
 /*   By: chbechet <chbechet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 23:12:35 by chbechet          #+#    #+#             */
-/*   Updated: 2017/02/25 21:37:39 by chbechet         ###   ########.fr       */
+/*   Updated: 2017/02/26 19:23:41 by chbechet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-static char     *ft_read(const int fd, char *str)
+
+static char		*ft_read(const int fd, char *str)
 {
-	int             ret;
-	char            buf[BUFF_SIZE + 1];
-	char            *tmp;
+	int			ret;
+	char		buf[BUFF_SIZE + 1];
+
 	ret = BUFF_SIZE;
 	while (!str || (ft_strchr(str, '\n') == NULL && ret == BUFF_SIZE))
 	{
 		if ((ret = read(fd, buf, BUFF_SIZE)) == -1)
 			return (NULL);
 		buf[ret] = '\0';
-		if(!str)
+		if (!str)
 			str = ft_strdup(buf);
 		else
 			str = ft_strjoin(str, buf);
 	}
 	return (str);
 }
-static int  ft_last_line(char **str, char **line)
+
+int				get_next_line(const int fd, char **line)
 {
-	*line = ft_strdup(*str);
-	free(*str);
-	*str = NULL;
-	return (1);
-}
-int     get_next_line(const int fd, char **line)
-{
-	static char     *str = NULL;
-	char            *tmp;
-	int             i;
-	i = 0;
+	static char		*str = NULL;
+	char			*tmp;
+
 	if (fd < 0)
 		return (-1);
 	if ((str = ft_read(fd, str)) == NULL)
 		return (-1);
-	while (str[i] && str[i] != '\n')
-		i++;
 	if ((tmp = ft_strchr(str, '\n')) != NULL)
 	{
 		*tmp = '\0';
 		*line = ft_strdup(str);
-		str = &str[i + 1];
+		str = tmp + 1;
 		return (1);
 	}
 	if (str && *str)
-		return (ft_last_line(&str, line));
-	return(0);
+	{
+		*line = ft_strdup(str);
+		ft_strdel(&str);
+		return (1);
+	}
+	return (0);
 }
