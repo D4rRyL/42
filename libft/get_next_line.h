9cf570743b09bf83fbda501f21d0c429956b5459
chbechet <chbechet@student.42.fr>
2017-03-19 15:35:30 +0100

    Add ft_error

---
+++ libft/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chbechet <chbechet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/08 23:12:07 by chbechet          #+#    #+#             */
+/*   Updated: 2017/03/09 07:39:44 by chbechet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 256
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
