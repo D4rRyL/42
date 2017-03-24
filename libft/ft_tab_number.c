767aa2b0cfc0db0e748c1379f2e65dfc29313d15
chbechet <chbechet@student.42.fr>
2017-03-24 11:46:02 +0100

    Normed

--- libft/ft_tab_number.c
+++ libft/ft_tab_number.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tab_number.c                                       :+:      :+:    :+:   */
+/*   ft_tab_number.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chbechet <chbechet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chbechet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/22 14:54:27 by chbechet          #+#    #+#             */
-/*   Updated: 2017/03/24 11:19:58 by chbechet         ###   ########.fr       */
+/*   Created: 2017/03/24 11:43:14 by chbechet          #+#    #+#             */
+/*   Updated: 2017/03/24 11:45:24 by chbechet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
+#include "libft.h"
 
-void	ft_tab_number(int **tab, int nb_l, int nb_col)	
+void	ft_tab_number(int **tab, int nb_l, int nb_col)
 {
 	int i;
 	int j;
